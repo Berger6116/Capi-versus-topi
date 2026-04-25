041a838be98a56c224ba93c425bb45c734ff471d
Yanina Tiribelli <yaninatiribelli@gmail.com>
2026-04-25 17:59:57 -0300

    actualizacion del gameManager

--- capi-versus-topi/Scrips/game_manager.gd
+++ capi-versus-topi/Scrips/game_manager.gd
@@ -1,20 +1,38 @@
 extends Node
 
+#VARIABLES GLOBALES
 var vidas_capi: int = 3
-var topos_derrotados= 0
+var topos_derrotados: int = 0
+var monedas: int = 0
 
+#SEÑALES
+signal actualizar_vidas(cantidad: int)
+signal actualizar_topos(cantidad: int)
+signal actualizar_monedas(cantidad: int)
+signal game_over
+
+#FUNCIONES
 func restar_vida():
-	vidas_capi-= 1
-	print("¡Ho no, Capi perdió una vida! le quedan: ",vidas_capi)
+	vidas_capi -= 1
+	actualizar_vidas.emit(vidas_capi)
+	print("¡oh no, Capi perdió una vida! le quedan: ",vidas_capi)
 	
 	if vidas_capi <= 0:
 		activar_game_over()
 		
-func sumar_puntaje(puntos: int):
-	topos_derrotados += puntos
-	prints("topos derrotados: ", topos_derrotados)
+#SEPARO EL PUNTAJE DE TOPOS DERROTADOS DE LAS MONEDAS QUE VA A DAR LA HUERTA
+func sumar_puntaje_topos():
+	topos_derrotados += 1
+	actualizar_topos.emit(topos_derrotados)
+	print("topos derrotados: ", topos_derrotados)
+	
+func sumar_monedas_huerta(cantidad: int):
+	monedas += cantidad
+	actualizar_monedas.emit(monedas)
+	print("Monedas Actuales: ", monedas)
 
 func activar_game_over():
-	print("Ho no, los topos derrotaron a Capi ")
+	game_over.emit()
+	print("oh no, los topos derrotaron a Capi")
 	
 			
