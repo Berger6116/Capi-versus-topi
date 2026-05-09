5b00670dbec373c0a964c061ca3ba8a7edb7e29f
Yanina Tiribelli <yaninatiribelli@gmail.com>
2026-05-09 20:12:40 -0300

    puliendo nivel 1

--- capi-versus-topi/Escenas/nivel_1.gd
+++ capi-versus-topi/Escenas/nivel_1.gd
@@ -4,21 +4,26 @@ extends Node2D
 @onready var camara: Camera2D = $Camera2D
 @onready var spawner_topos: Marker2D = $SpawnerTopos
 @onready var mensajes: Label = $Mensajes/Mensajes_nivel
+#Acá despues si va mas ordenado en un HUD se cambia esto:
+@onready var botones_huerta = $MundoHuertaPrueba/BotonesHuerta/ContenedorFade
 
 func _ready() -> void:
 		
 	GameManager.actualizar_topos.connect(chequear_victoria)
 	
 	#Fases del nivel 1: Huerta y luego oleada de topos
-	await fase_huerta(15.0)
-	await fase_oleada(3.0)
+	await fase_huerta(20.0)
+	await fase_oleada(5.0)
 	
 	#funcion movimiento de camara para iniciar oleada de topos
 	animar_camara_inicio()
 	
 	
 func animar_camara_inicio():
+	#fade de botones huerta
+	await fade_out(botones_huerta, 0.5)
 	
+	#movimiento de cámara:
 	var posicion_original = camara.position
 	var posicion_derecha = posicion_original + Vector2(1600, 0)
 	var animacion_camara = create_tween()
@@ -28,6 +33,10 @@ func animar_camara_inicio():
 	animacion_camara.tween_interval(1.0)
 	animacion_camara.tween_property(camara, "position", posicion_original, 2.0).set_trans(Tween.TRANS_SINE)
 	
+	await animacion_camara.finished
+	
+	#fade in, vuelven los botones huerta:
+	fade_in(botones_huerta, 0.5)
 
 func mostrar_mensaje(msj: String) -> void:
 	mensajes.text = msj
@@ -44,7 +53,21 @@ func fase_oleada (tiempo: float) -> void:
 	spawner_topos.get_node("TimerOleada").start()	
 	await get_tree().create_timer(tiempo).timeout
 	
+func fade_out(nodo: Node, tiempo: float) ->void:
+	nodo.show()
+	var fadeOut = create_tween()
+	fadeOut.tween_property(nodo, "modulate:a", 0.0, tiempo).set_trans(Tween.TRANS_SINE)
+	await fadeOut.finished
+	nodo.hide()
 	
+func fade_in(nodo: Node, tiempo: float) ->void:
+	nodo.modulate.a = 0.0
+	nodo.show()
+	var fadeIn = create_tween()
+	fadeIn.tween_property(nodo, "modulate:a", 1.0, tiempo).set_trans(Tween.TRANS_SINE)
+	
+	
+
 func chequear_victoria (topos_muertos: int) -> void:
 	
 	if topos_muertos >= 10:
