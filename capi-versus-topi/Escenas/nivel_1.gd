8e77495a396c4a68d2ad78457ee405fa63049e10
Yanina Tiribelli <yaninatiribelli@gmail.com>
2026-05-09 18:47:34 -0300

    fase huerta y fase oleada version 1

--- capi-versus-topi/Escenas/nivel_1.gd
+++ capi-versus-topi/Escenas/nivel_1.gd
@@ -3,13 +3,18 @@ extends Node2D
 #NIVEL 1
 @onready var camara: Camera2D = $Camera2D
 @onready var spawner_topos: Marker2D = $SpawnerTopos
-@onready var victoria: CanvasLayer = $Victoria
+@onready var mensajes: Label = $Mensajes/Mensajes_nivel
 
 func _ready() -> void:
-	#funcion movimiento de camara
-	animar_camara_inicio()
-	
+		
 	GameManager.actualizar_topos.connect(chequear_victoria)
+	
+	#Fases del nivel 1: Huerta y luego oleada de topos
+	await fase_huerta(15.0)
+	await fase_oleada(3.0)
+	
+	#funcion movimiento de camara para iniciar oleada de topos
+	animar_camara_inicio()
 	
 	
 func animar_camara_inicio():
@@ -23,21 +28,32 @@ func animar_camara_inicio():
 	animacion_camara.tween_interval(1.0)
 	animacion_camara.tween_property(camara, "position", posicion_original, 2.0).set_trans(Tween.TRANS_SINE)
 	
-	animacion_camara.finished.connect(_on_paneo_finalizado)
+
+func mostrar_mensaje(msj: String) -> void:
+	mensajes.text = msj
+	mensajes.show()
+	await get_tree().create_timer(3.0).timeout
+	mensajes.hide()
+
+func fase_huerta (tiempo: float) -> void:
+	mostrar_mensaje("Tienes 15 segundos para plantar!!!")
+	await get_tree().create_timer(tiempo).timeout
+
+func fase_oleada (tiempo: float) -> void:
+	mostrar_mensaje("Cuidado que vienen los topos!!!")
+	spawner_topos.get_node("TimerOleada").start()	
+	await get_tree().create_timer(tiempo).timeout
 	
-func _on_paneo_finalizado() -> void:
-	#encendemos el spawner cuando termina el paneo de cámara inicial
-	spawner_topos.get_node("TimerOleada").start()
-	print("inicia el spawner de topos")
 	
 func chequear_victoria (topos_muertos: int) -> void:
 	
 	if topos_muertos >= 10:
-		print("Lograste la victoria! mataste 10 topos!")
+		#print("Lograste la victoria! mataste 10 topos!")
 		var timer = spawner_topos.get_node("TimerOleada")
 		if timer != null:
 			timer.stop()
 		#señal para eliminar topos restantes:
 		spawner_topos.eliminar_topos_restantes.emit()
 		#mostrar cartel de victoria:
-		victoria.show()
+		mensajes.text = "Nivel 1 completado!!!"
+		mensajes.show()
