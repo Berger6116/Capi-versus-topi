ddd5ee8497dd58e8933f17bf61dfb4c44001f6fa
annabellamazzello125690-max <annabellamazzello125690@gmail.com>
2026-06-21 21:48:04 -0300

    nuevos botones

--- capi-versus-topi/menu/munu.gd
+++ capi-versus-topi/menu/munu.gd
@@ -11,6 +11,5 @@ func _on_texture_button_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_setting_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu_setting.tscn")
