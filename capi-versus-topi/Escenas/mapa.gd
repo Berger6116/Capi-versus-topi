000ab8c7df09e71f670bb0f901ac23ea46221d57
Yanina Tiribelli <yaninatiribelli@gmail.com>
2026-06-22 21:56:43 -0300

    resetear niveles desde la tienda

--- capi-versus-topi/Escenas/mapa.gd
+++ capi-versus-topi/Escenas/mapa.gd
@@ -5,8 +5,10 @@ func _on_back_pressed() -> void:
 
 
 func _on_boton_1_pressed() -> void:
+	GameManager.resetear_para_nuevo_nivel()
 	get_tree().change_scene_to_file("res://Escenas/nivel_1.tscn")
 
 
 func _on_boton_2_pressed() -> void:
+	GameManager.resetear_para_nuevo_nivel()
 	get_tree().change_scene_to_file("res://Escenas/nivel_2.tscn")
